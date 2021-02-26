39d632f9eda95ca25ad87fd52426cbe9077961b2
Soumili1818 <74952491+Soumili1818@users.noreply.github.com>
2021-02-26 23:48:46 +0530

    Add files via upload

---
+++ story.css
@@ -0,0 +1,19 @@
+header{
+    text-align: center;
+    font-size: 200%;
+    
+   
+    
+}
+body,html{
+    height: 100%;
+    background: url("a.jpg");
+    background-repeat: no-repeat;
+    top:0;
+    background-size:cover;
+    background-position:center;
+    background-color: cyan;
+    margin: 0;
+    padding: 0;
+   
+}
